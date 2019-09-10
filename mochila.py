7f714dbdbd5456f84b57695148c42f376706b5a6
alusterax <38334986+alusterax@users.noreply.github.com>
2019-09-10 17:58:59 -0300

    Update mochila.py

--- mochila.py
+++ mochila.py
@@ -1,7 +1,8 @@
 from artigo import Artigo
 import random
 import numpy
-
+##Para melhores resultados, utilizar o método elitista (nao obtive muito sucesso usando a roleta)
+##
 volumes = [6,2,1,8,2,3,5,9,8,8,6,2,7,9,7,10,2,9,1,9,3,9,9,2,4]
 pesos = [2,9,9,7,2,2,3,4,4,7,7,3,7,7,3,4,10,2,5,8,10,2,8,6,7]
 valores = [5,7,8,9,2,6,2,1,8,1,8,10,9,6,2,8,6,5,2,7,8,7,9,5,6]
